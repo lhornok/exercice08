546edbc82a1862a7ad40036ed2c0d26c8b0fef59
lhornok <lhornok@gmail.com>
2020-09-30 07:42:36 +0000

    add build folder

---
+++ build/prestashop/files/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mysqldb',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'prestashopuser',
+    'database_password' => '+QAY2wsx3edc',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'pSGLKVng1wQ0cKTa7MrV55H7051ODMPoiO9Wifu1WCYSyKf15EW3DJUG',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2020-09-12',
+    'locale' => 'fr-FR',
+    'cookie_key' => 'r0LjFkhIaf1HADhNiqHwHCFI3DwiLVsaZLQXiL3NLs2dgH71Pcp0RVQ2',
+    'cookie_iv' => 'fpBavQLX',
+    'new_cookie_key' => 'def000009a14fc4db37df9af68c190f9c5c8a230f9377fa27d1cc89c69473598584f630175dedf6766280b6495f8d754b96cab8c77ce3b6f395d3acf99895a7116ddb23f',
+  ),
+);
