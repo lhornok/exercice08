f25ec888d66bc5d9935d9bd7171fbbc36b750241
lhornok <lhornok@gmail.com>
2020-10-10 17:39:37 +0000

    modif

--- init.sh
+++ init.sh
@@ -3,16 +3,7 @@
 sleep 20
 wget http://localhost:8002/prestashop/fr/ -o /tmp/1.log -O /tmp/index.html
 wget 'http://localhost:8002/prestashop/fr/recherche?controller=search&s=renard' -o /tmp/2.log -O /tmp/search.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/30.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/31.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/32.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/33.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/34.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/35.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/36.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/37.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/38.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/39.log /tmp/coussin.html
-wget http://localhost:8002/prestashop/fr/panier?action=show -o /tmp/4.log /tmp/panier.html
+wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/30.log -O /tmp/coussin.html
+wget http://localhost:8002/prestashop/fr/panier?action=show -o /tmp/4.log -O /tmp/panier.html
 
 rm -f /tmp/result.xml
