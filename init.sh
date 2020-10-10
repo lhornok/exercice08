56329cc517e00c16ac779c0adff7b53e55a4710a
lhornok <lhornok@gmail.com>
2020-10-10 13:20:09 +0000

    ajout script init

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+wget http://localhost:8002/prestashop/fr/ -o /tmp/1.log -O /tmp/index.html
+wget 'http://localhost:8002/prestashop/fr/recherche?controller=search&s=renard' -o /tmp/2.log -O /tmp/search.html
+wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/3.log /tmp/coussin.html
+wget http://localhost:8002/prestashop/fr/panier?action=show -o /tmp/4.log /tmp/panier.html
+
+rm -f /tmp/result.xml
