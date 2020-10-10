f29bf0b831584fd5b4694809dbf5fc9182f4306f
lhornok <lhornok@gmail.com>
2020-10-10 18:14:19 +0000

    Adapt ip

--- docker/dump/update.sql
+++ docker/dump/update.sql
@@ -1,1 +1,1 @@
-update ps_shop_url set domain="localhost:8002", domain_ssl="localhost:8002" where main=1 and domain="192.168.34.30";
+update ps_shop_url set domain="192.168.34.30:8002", domain_ssl="192.168.34.30:8002" where main=1 and domain="192.168.34.30";
