a08b0a5814494dd2e09f286f5c274a1186556c0e
lhornok <lhornok@gmail.com>
2020-10-10 15:21:23 +0000

    erreur dans le nom du lien

--- tests/locators.py
+++ tests/locators.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators(object):
     """A class for main page locators. All main page locators should come here"""
     S_FIELD = (By.NAME, 's')
-    ARTICLE_LINK = (By.PARTIAL_LINK_TEXT, 'Coussin')
+    ARTICLE_LINK = (By.PARTIAL_LINK_TEXT, 'coussin')
     ARTICLE_QTY = (By.ID, 'quantity_wanted')
     CART_LINK = (By.XPATH, 'html/body/main/section/div/div/section/div[1]/div[2]/div[2]/div[2]/form/div[2]/div/div[2]/button')
     CART_DISPLAY_LINK = (By.XPATH, '/html/body/main/header/nav/div/div/div[1]/div[2]/div[3]/div/div/a')
