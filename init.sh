fc028e20f0b613331220404402124b8e9e06fd5e
lhornok <lhornok@gmail.com>
2020-10-10 13:21:28 +0000

    Add sleep

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sleep 20
 wget http://localhost:8002/prestashop/fr/ -o /tmp/1.log -O /tmp/index.html
 wget 'http://localhost:8002/prestashop/fr/recherche?controller=search&s=renard' -o /tmp/2.log -O /tmp/search.html
 wget http://localhost:8002/prestashop/fr/accessoires-de-maison/9-coussin-renard.html -o /tmp/3.log /tmp/coussin.html
